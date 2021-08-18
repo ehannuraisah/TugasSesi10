ceebd72ed1491217746dc8856783ae2f3109ca96
Project 1 Input Nama <ehannuraisah.a4@gmail.com>
2021-08-18 10:27:17 +0700

    Tugas Sesi 10

---
+++ app/src/main/java/com/example/tugassesi10/DatabaseHelper.java
@@ -0,0 +1,60 @@
+package com.example.tugassesi10;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.util.Log;
+import java.util.ArrayList;
+public static String DATABASE_NAME ="student_database";
+private static final int DATABASE_VERSION = 1;
+private static final String TABLE_STUDENTS = "students";
+private static final String KEY_ID = "id";
+private static final String KEY_FIRSTNAME = "name";
+
+private static final String CREATE_TABLE_STUDENTS = "CREATE TABLE"
++ TABLE_STUDENTS + "(" + KEY_ID
++ "INTEGER PRIMARY KEY AUTOINCREMENT," + KEY_FIRSTNAME + "TEXT );";
+
+public class DatabaseHelper(Context context) {
+    super (contex, DATABASE_NAME, null, DATABASE_VERSION);
+
+    Log.d("table", CREATE_TABLE_STUDENTS);
+}
+@Override
+public void onCreate(SQLiteDatabase db){
+    db.execSQL(CREATE_TABLE_STUDENTS);
+}
+@Override
+public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+    db.execSQL("DROP TABLE IF EXISTS'" + TABLE_STUDENTS +"'");
+    onCreate(db);
+}
+public long addStudentDetail(String student){
+    SQLiteDatabase db = this.getWritableDatabase();
+    // creating content value
+    ContentValues values = new ContentValues();
+    values.put(KEY_FISTNAME, student);
+    // insert row in studnets table
+    long insert = db.insert(TABLE_STUDENTS, null, values);
+
+    return  insert;
+}
+public ArrayList<String> getAllStudentsList(){
+    ArrayList<String> studentsArrayList = new ArrayList<String>();
+    String name ="";
+    String selecQuery = "SELECT * FROM" + TABLE_STUDENTS;
+    SQLiteDatabase db = this.getReadableDatabase();
+    Cursor c = db.rawQuery(selecQuery, null);
+    // looping through all raws and adding to list
+    if (c.moveToFirst()){
+        do {
+            name = c.getString(c.getColumnIndex(KEY_FIRSTNAME));
+            // adding to studnts list
+            studentsArrayList.add(name);
+        }while (c.moveToNext());
+        Log.d("array", studentsArrayList.toString());
+    }
+    return studentsArrayList;
+}
